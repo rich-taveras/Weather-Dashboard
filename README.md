65bc69237aab72575b79e29624f613ebcad6854b
rich-taveras <rtaveras@yahoo.com>
2023-07-27 23:58:22 -0400

    updated HTML

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # Weather-Dashboard
+
+## Description
+
+Your Weather Dashboard is a web application that allows users to search for weather information of different cities. It provides current weather data, including temperature, wind speed, and humidity, as well as a 5-day forecast for the selected city.
+
+## Features
+
+- Search for weather data of any city around the world.
+- Display current weather information, including temperature, wind speed, and humidity.
+- Show a 5-day forecast for the selected city.
+- Visual representation of weather icons to indicate different weather conditions.
+
+## Technologies Used
+
+- HTML5, CSS3, JavaScript
+- [OpenWeatherMap API](https://openweathermap.org/api) - API for weather data retrieval
+- [Day.js](https://day.js.org/) - Library for date and time manipulation
+
+## Installation
+
+1. Clone the repository:
+
+git clone git@github.com:rich-taveras/Weather-Dashboard.git
+
+2. Open the index.html file in your preferred web browser.
+
+## Usage
+
+1. Enter the name of the city you want to get weather information for in the search input field.
+
+2. Click on the "Search" button.
+
+3. The current weather information and the 5-day forecast for the selected city will be displayed.
+
+hereis the link:
+https://rich-taveras.github.io/Weather-Dashboard/
+
+## Screenshots
+
+<img src="./assets/images/Screenshot 2023-07-27 234929.png">
+
+## Demo
+
+N/A
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Credits
+
+- Weather icons provided by [OpenWeatherMap](https://openweathermap.org/weather-conditions)
+
+## Contact
+
+For any inquiries or suggestions, please contact [taverasr@gmail.com].
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,103 +1,58 @@
-// script.js
+var apiKey = "f4c48b2431b2e1f893e3a6845dcf023c";
+var titleEl = document.getElementById("title");
+var tempEl = document.getElementById("temp");
+var windEl = document.getElementById("wind");
+var humidityEl = document.getElementById("humidity");
+var searchBtn = document.getElementById("search-btn");
+var cityInput = document.getElementById("city-input");
 
-$(document).ready(function () {
-    const apiKey = 'f4c48b2431b2e1f893e3a6845dcf023c' ;
-  
-    // Function to fetch weather data from the API
-    function fetchWeatherData(city) {
-      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-  
-      $.ajax({
-        url: apiUrl,
-        method: 'GET',
-        dataType: 'json',
-        success: function (data) {
-          // Update the weather information in the dashboard
-          updateWeatherData(data);
-        },
-        error: function (error) {
-          console.log('Error fetching weather data:', error);
-        }
-      });
-    }
-  
-    // Function to update weather information in the dashboard
-    function updateWeatherData(data) {
-      $('#name_of_city').text(data.name);
-      $('#today_temp').text(`Temperature: ${data.main.temp} °C`);
-      $('#today_humidity').text(`Humidity: ${data.main.humidity}%`);
-      $('#today_wind_speed').text(`Wind Speed: ${data.wind.speed} m/s`);
-      $('#today_icon_div').attr('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
-    }
-  
-    // Function to fetch five-day forecast data from the API
-    function fetchFiveDayForecast(city) {
-      const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
-  
-      $.ajax({
-        url: apiUrl,
-        method: 'GET',
-        dataType: 'json',
-        success: function (data) {
-          // Update the five-day forecast information in the dashboard
-          updateFiveDayForecast(data);
-        },
-        error: function (error) {
-          console.log('Error fetching five-day forecast data:', error);
-        }
-      });
-    }
-  
-    // Function to update five-day forecast information in the dashboard
-    function updateFiveDayForecast(data) {
-      for (let i = 0; i < 5; i++) {
-        const date = new Date(data.list[i].dt_txt).toDateString();
-        $(`#${i}date`).text(date);
-        $(`#day${i}five_day_temp`).text(`Temperature: ${data.list[i].main.temp} °C`);
-        $(`#day${i}five_day_humidity`).text(`Humidity: ${data.list[i].main.humidity}%`);
-        $(`#day${i}five_day_icon`).attr('src', `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png`);
-      }
-    }
-  
-    // Event handler for the search button
-    $('#searchbtn').on('click', function (event) {
-      event.preventDefault();
-      const city = $('#search_bar').val().trim();
-  
-      // Fetch current weather data and five-day forecast for the city
-      fetchWeatherData(city);
-      fetchFiveDayForecast(city);
-    });
-  });
+function searchCity() {
+  var cityName = cityInput.value;
 
-  $(document).ready(function () {
-    // Arreglo para almacenar las ciudades agregadas
-    const addedCities = [];
-  
-    // Evento click para el botón de agregar ciudades
-    $('#add_city_btn').on('click', function () {
-      // Obtiene el nombre de la ciudad ingresado por el usuario
-      const city = $('#search_bar').val().trim();
+  displayWeather(cityName);
+}
+
+function displayWeather(cityName) {
+  var url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&appid=" +
+    apiKey +
+    "&units=imperial";
+
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (currentData) {
+      console.log(currentData);
+      titleEl.innerHTML = currentData.name + "<img src='https://openweathermap.org/img/wn/" + currentData.weather[0].icon + "@2x.png'>"
       
-      // Verifica si la ciudad ya ha sido agregada
-      if (city && !addedCities.includes(city)) {
-        addedCities.push(city);
-        // Crea y agrega el botón para la nueva ciudad
-        const newCityBtn = $('<button class="btn btn-outline-light my-2 my-sm-0 city-btn"></button>').text(city);
-        $('.buttons_div').append(newCityBtn);
+      //+ dayjs.unix(currentData.dt).format(" (MM/DD/YYYY)") +
+    });
+    
+    
+
+  var forecastUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityName +
+    "&appid=" +
+    apiKey +
+    "&units=imperial";
+
+  fetch(forecastUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (forecastData) {
+      console.log(forecastData);
+
+      //grab every 12pm for each day for 5 days
+      var forecastArray = forecastData.list;
+      for (let i = 3; i < forecastArray.length; i = i + 8) {
+        console.log(forecastArray[i]);
       }
-  
-      // Limpia el campo del search bar
-      $('#search_bar').val('');
     });
-  
-    // Evento click para los botones de las ciudades agregadas
-    $(document).on('click', '.city-btn', function () {
-      const city = $(this).text();
-      // Fetch current weather data and five-day forecast for the city
-      fetchWeatherData(city);
-      fetchFiveDayForecast(city);
-    });
-  });
-  
-  
+}
+
+searchBtn.addEventListener("click", searchCity);
