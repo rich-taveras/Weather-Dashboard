de66018f7b87b21799cac3167be4563cd197d883
rich-taveras <rtaveras@yahoo.com>
2023-07-28 21:17:13 -0400

    added to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 var apiKey = "f4c48b2431b2e1f893e3a6845dcf023c";
-var titleEl = document.getElementById("title");
+var cardTitleEl = document.getElementById("card-title");
 var tempEl = document.getElementById("temp");
 var windEl = document.getElementById("wind");
 var humidityEl = document.getElementById("humidity");
 var searchBtn = document.getElementById("search-btn");
 var cityInput = document.getElementById("city-input");
-var fivedayForecastEl=document.getElementById("fiveda-forecast")
+var fivedayForecastEl = document.getElementById("fiveda-forecast");
 
 function searchCity() {
   var cityName = cityInput.value;
@@ -27,12 +27,13 @@ function displayWeather(cityName) {
     })
     .then(function (currentData) {
       console.log(currentData);
-      titleEl.innerHTML = currentData.name + dayjs.unix(currentData.dt).format(" (MM/DD/YYYY)") + "<img src='https://openweathermap.org/img/wn/" + currentData.weather[0].icon + "@2x.png'>"
-      
-      //+ dayjs.unix(currentData.dt).format(" (MM/DD/YYYY)") +
+      cardTitleEl.innerHTML =
+        currentData.name +
+        dayjs.unix(currentData.dt).format(" (MM/DD/YYYY)") +
+        "<img src='https://openweathermap.org/img/wn/" +
+        currentData.weather[0].icon +
+        "@2x.png'>";
     });
-    
-    
 
   var forecastUrl =
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
@@ -50,16 +51,23 @@ function displayWeather(cityName) {
 
       //grab every 12pm for each day for 5 days
       var forecastArray = forecastData.list;
-     
-      for (let i = 3; i < forecastArray.length; i = i + 8,j++) {
+
+      for (let i = 3, j = 1; i < forecastArray.length; i = i + 8, j++) {
         console.log(forecastArray[i]);
-        var cardTitle=document.getElementById("card-title"+j)
-        console.log("card-title"+j)
-        cardTitle.textContent=dayjs.unix(forecastArray[i].dt).format(" (MM/DD/YYYY)")
-     
-           }
+        var cardTitleEl = document.getElementById("card-title" + j);
+        console.log("card-title" + j);
+        cardTitleEl.textContent = dayjs
+          .unix(forecastArray[i].dt)
+          .format(" (MM/DD/YYYY)");
+        var temp = document.getElementById("temp" + j);
+
+        temp.textContent = forecastArray[i].main.temp;
+
+        var wind = document.getElementById("wind" + j);
+
+        wind.textContent = forecastArray[i].wind.speed;
+      }
     });
 }
 
 searchBtn.addEventListener("click", searchCity);
-
